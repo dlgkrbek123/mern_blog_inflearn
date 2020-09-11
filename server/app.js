a0c04f327e4f3a6bb40e38ec82b799b4a836560a
hakyulee <dlgkrbek123@cashwalk.io>
2020-09-11 15:42:41 +0900

    몽고 db 클러스터 연결

--- server/app.js
+++ server/app.js
@@ -1,7 +1,37 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
+import hpp from 'hpp';
+import helmet from 'helmet';
+
+const { MONGO_URI } = config;
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connecting Success'))
+  .catch((e) => {
+    console.log(e);
+  });
 
 const app = express();
 
+app.use(hpp());
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: true, // 모두 허용
+    credentials: true, // 브라우져헤더에 두가지를 추가하게 해줌
+  })
+);
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+// Use routes
 app.get('/', (req, res) => {
   res.send('엄준식');
 });
---
+++ server/routes/api/post.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import Post from '../../models/post';
+
+const router = express.Router();
+
+// /api/post
+router.get('/', async (req, res) => {});
