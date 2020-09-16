f44f014393bf97fd27c7031db4a72af26dd34e7e
hakyulee <dlgkrbek123@cashwalk.io>
2020-09-16 13:23:23 +0900

    라우트 기본 설정 추가

--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -2,13 +2,33 @@ import React from 'react';
 import AppNavBar from '../components/AppNavbar';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { Container } from 'reactstrap';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import PostCardResult from './normalRoute/PostCardResult';
+import PostWrite from './normalRoute/PostWrite';
+import PostDetail from './normalRoute/PostDetail';
+import Search from './normalRoute/Search';
+import CategoryResult from './normalRoute/CategoryResult';
 
 const MyRouter = () => {
   return (
     <>
       <AppNavBar />
       <Header />
-      <h1>Hello Body</h1>
+      <Container id="main-body">
+        <Switch>
+          <Route path="/" exact component={PostCardResult} />
+          <Route path="/post" exact component={PostWrite} />
+          <Route path="/post/:id" exact component={PostDetail} />
+          <Route
+            path="/post/category/:categoryName"
+            exact
+            component={CategoryResult}
+          />
+          <Route path="/search/:searchTerm" exact component={Search} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </Container>
       <Footer />
     </>
   );
---
+++ client/src/routes/normalRoute/PostCardResult.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostCardResult = () => {
+  return <h1>PostCardResult</h1>;
+};
+
+export default PostCardResult;
