e71131cb298127385032fc6619102a32849bf151
hakyulee <dlgkrbek123@cashwalk.io>
2020-09-16 19:05:34 +0900

    에디터 마크업 작성

--- client/src/routes/normalRoute/PostWrite.js
+++ client/src/routes/normalRoute/PostWrite.js
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import { editorConfiguration } from '../../components/Editor/config';
+import MyInit from '../../components/Editor/UploadAdapter';
 
 const PostWrite = () => {
-  return <h1>PostWrite</h1>;
+  const dispatch = useDispatch();
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  const [form, setValues] = useState({
+    title: '',
+    contents: '',
+    fileUrl: '',
+  });
+
+  const onChange = (e) => {
+    setValues({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const { title, contents, fileUrl, category } = form;
+  };
+
+  return (
+    <div>
+      {isAuthenticated && (
+        <Form>
+          <FormGroup className="mb-3">
+            <Label for="title">Title</Label>
+            <Input
+              type="text"
+              id="title"
+              name="title"
+              className="form-control"
+              onChange={onChange}
+            />
+          </FormGroup>
+          <FormGroup className="mb-3">
+            <Label for="category">Category</Label>
+            <Input
+              type="text"
+              id="category"
+              name="category"
+              className="form-control"
+              onChange={onChange}
+            />
+          </FormGroup>
+          <FormGroup className="mb-3">
+            <Label for="content">Content</Label>
+            <CKEditor
+              editor={ClassicEditor}
+              config={editorConfiguration}
+              onInit={MyInit}
+            />
+          </FormGroup>
+        </Form>
+      )}
+    </div>
+  );
 };
 
 export default PostWrite;
