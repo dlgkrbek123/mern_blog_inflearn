cf6309175b2928daf950d172fd909e3cee217c32
hakyulee <dlgkrbek123@cashwalk.io>
2020-09-16 18:21:04 +0900

    이미지 업로드 라우터 작성

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -2,6 +2,33 @@ import express from 'express';
 import Post from '../../models/post';
 import auth from '../../middleware/auth';
 
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import path from 'path';
+import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_PRIVATE_KEY,
+});
+
+const uploadS3 = multer({
+  storage: multerS3({
+    s3,
+    bucket: 'sideprojectinflearn',
+    region: 'ap-northeast-2',
+    key(req, file, cb) {
+      const ext = path.extname(file.originalname);
+      const basename = path.basename(file.originalname, ext);
+
+      cb(null, basename + new Date().valueOf() + ext);
+    },
+  }),
+  limits: { fileSize: 100 * 1024 * 1024 },
+});
+
 const router = express.Router();
 
 router.get('/', async (req, res) => {
@@ -24,6 +51,19 @@ router.post('/', auth, async (req, res, next) => {
     res.json(newPost);
   } catch (e) {
     console.log(e);
+  }
+});
+
+router.post('/image', uploadS3.array('upload', 5), async (req, res, next) => {
+  try {
+    console.log(req.files.map((v) => v.location));
+    res.json({
+      uploaded: true,
+      url: req.files.map((v) => v.location),
+    });
+  } catch (e) {
+    console.error(e);
+    res.json({ uploaded: false, url: null });
   }
 });
 
